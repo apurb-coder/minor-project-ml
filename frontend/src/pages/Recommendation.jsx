af4fcdd7176648a883d32294f604b20b050dfb7f
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-03-30 00:05:18 +0530

    Update Recommendation page with Spotify link and hover effect

--- frontend/src/pages/Recommendation.jsx
+++ frontend/src/pages/Recommendation.jsx
@@ -1,10 +1,11 @@
 import React,{useState} from 'react'
 import ytlogo from "../assets/youtubelogo.png"
 import spotifylogo from "../assets/Spotify_Logo_CMYK_Green.png"
+import { redirect } from 'react-router-dom';
 
 const Recommendation = () => {
-  const [spotifylink, setSpotifylink] = useState("https://open.spotify.com/");
   const [youtubelink, setYoutubelink] = useState("https://www.youtube.com/");
+  const [spotifylink, setSpotifylink] = useState("https://open.spotify.com/");
   return (
     <div className=" font-medium">
       <div className="flex flex-col justify-center item-center p-[1.25rem]">
@@ -13,23 +14,15 @@ const Recommendation = () => {
       <div className="flex flex-col justify-center  p-[1.25rem]">
         <h3 className="text-xl">Recommendation</h3>
         <div className="mt-9 flex">
-          <div className="mr-3 px-5 py-3 bg-white rounded-full w-1/2">
+          <div className="mr-3 px-5 py-3 bg-white rounded-full w-1/2 flex justify-center items-center hover:cursor-pointer">
             <a href={youtubelink}>
-              <img
-                src={ytlogo}
-                alt="yt logo"
-                className="h-4 flex justify-center items-center"
-              />
+              <img src={ytlogo} alt="yt logo" className="h-4" />
             </a>
           </div>
 
-          <div className="ml-3 px-5 py-3 bg-white rounded-full w-1/2">
+          <div className="ml-3 px-5 py-3 bg-white rounded-full w-1/2 flex justify-center items-center hover:cursor-pointer">
             <a href={spotifylink}>
-              <img
-                src={spotifylogo}
-                alt="yt logo"
-                className="h-5 flex justify-center items-center"
-              />
+              <img src={spotifylogo} alt="yt logo" className="h-4" />
             </a>
           </div>
         </div>
