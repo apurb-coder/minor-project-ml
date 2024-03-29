cfbc592e22d99ce0475421a9f9ef818fedf4a950
apurb-coder <55659698+apurb-coder@users.noreply.github.com>
2024-03-29 23:56:13 +0530

    Add Spotify and YouTube links to Recommendation page

--- frontend/src/pages/Recommendation.jsx
+++ frontend/src/pages/Recommendation.jsx
@@ -1,6 +1,10 @@
-import React from 'react'
+import React,{useState} from 'react'
+import ytlogo from "../assets/youtubelogo.png"
+import spotifylogo from "../assets/Spotify_Logo_CMYK_Green.png"
 
 const Recommendation = () => {
+  const [spotifylink, setSpotifylink] = useState("https://open.spotify.com/");
+  const [youtubelink, setYoutubelink] = useState("https://www.youtube.com/");
   return (
     <div className=" font-medium">
       <div className="flex flex-col justify-center item-center p-[1.25rem]">
@@ -8,10 +12,30 @@ const Recommendation = () => {
       </div>
       <div className="flex flex-col justify-center  p-[1.25rem]">
         <h3 className="text-xl">Recommendation</h3>
-        <p>Based on your input, we recommend...</p>
+        <div className="mt-9 flex">
+          <div className="mr-3 px-5 py-3 bg-white rounded-full w-1/2">
+            <a href={youtubelink}>
+              <img
+                src={ytlogo}
+                alt="yt logo"
+                className="h-4 flex justify-center items-center"
+              />
+            </a>
+          </div>
+
+          <div className="ml-3 px-5 py-3 bg-white rounded-full w-1/2">
+            <a href={spotifylink}>
+              <img
+                src={spotifylogo}
+                alt="yt logo"
+                className="h-5 flex justify-center items-center"
+              />
+            </a>
+          </div>
+        </div>
       </div>
     </div>
-  )
+  );
 }
 
 export default Recommendation
